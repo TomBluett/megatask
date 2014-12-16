9e2dcc3b864726f97693eb161c590d63c45f41f1
Tom Bluett <tbluett@me.com>
2014-12-16 13:24:55 -0500

    Added delete buttons and updated local Storage to be consistent after refresh

--- js/megatask.js
+++ js/megatask.js
@@ -1,6 +1,7 @@
 var Megatask = function() {
   function Megatask() {
     this.tasks = [];
+    this.counter = 0;
     var self = this;
     var supportsStorage = function(){
       try {
@@ -22,9 +23,11 @@ var Megatask = function() {
     };
     var addTask = function(taskName, taskCompleted) {
         taskCompleted = !!taskCompleted;
+        self.counter++;
         var newTask = {
+          id: self.counter,
           name: taskName,
-          completed: taskCompleted
+          completed: taskCompleted,
         };
         self.tasks.push(newTask);
         var newItem = createListItem(newTask);
@@ -32,7 +35,13 @@ var Megatask = function() {
         saveTasks();
     };
     var createListItem = function(task) {
-      return '<li class="list-group-item">' + task.name + '</li>'
+      var deleteButton, editButton, buttonGroup, label;
+      label = '<label>' + task.name + '</label>';
+      deleteButton = '<button class="btn btn-sm btn-danger"><i class="fa fa-trash fa-lg"></i></button>';
+      editButton = '<button class="btn btn-sm btn-success edit">edit</button>';
+      buttonGroup = '<div class="btn-group">' + deleteButton + editButton + '</div>';
+      return '<li class="list-group-item" id="task_' + task.id + '""><div class="task">' + label +
+      buttonGroup + '</div></li>';
     };
     var saveTasks = function(){
       if (supportsStorage()){
@@ -40,11 +49,36 @@ var Megatask = function() {
       }
     };
 
+    var getListItemFromButton = function(button) {
+      return $(button).closest('li');
+    };
+
+    var getTaskIdFromListItem = function(listItem) {
+      var id = listItem.attr('id');
+      return id.substring(id.lastIndexOf('_') + 1);
+    };
+
     $('#new_task').submit(function(ev) {
       ev.preventDefault();
       var field = $(this.elements.task_name);
       addTask(field.val());
       field.val('');
+    });
+    $('#tasks').on('click', 'button.btn-danger', function() {
+      var listItem = getListItemFromButton(this);
+      var id = getTaskIdFromListItem(listItem);
+      for (var i=0; i < self.tasks.length; i++) {
+        if (self.tasks[i].id.toString() === id) {
+          self.tasks.splice(i, 1);
+        }
+      }
+      listItem.remove();
+      saveTasks();
+    });
+
+    $('#tasks').on('click', 'button.edit', function() {
+      var editForm = $('.edit_task.hidden').clone();
+
     });
 
     loadTasks();
